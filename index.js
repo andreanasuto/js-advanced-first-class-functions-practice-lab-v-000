b912d5a20ab1c671a454b442ed49690f91f7dd50
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-03-03 00:30:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // Code your solution in this file!
 function logDriverNames (drivers) {
-  console.log(drivers);
-  drivers.forEach(console.log(name));
+  return drivers.forEach(console.log(name));
 }
