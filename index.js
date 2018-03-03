a210fc7c80e44e8bf4fae46577f43f8b919f8e77
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-03-03 00:48:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function logDriversByHometown(array, location) {
 }
 
 function driversByRevenue(array) {
-  array.slice().sort(function (a, b) {
+  const newarray = array.slice().sort(function (a, b) {
   return a.revenue - b.revenue;
   })
 }
